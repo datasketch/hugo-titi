a53855376aab8ec0cb79246bcb155c6a1b97a095
David Daza <03dazal@gmail.com>
2018-11-21 16:59:16 -0500

    Navbar and footer rendered by configuration params

---
+++ static/js/base.js
@@ -0,0 +1,9 @@
+const burger = document.querySelector('.burger')
+const burgerMenu = document.querySelector('.burger-menu')
+
+if (burger) {
+  burger.addEventListener('click', function () {
+    this.classList.toggle('open')
+    burgerMenu.classList.toggle('open')
+  })
+}
